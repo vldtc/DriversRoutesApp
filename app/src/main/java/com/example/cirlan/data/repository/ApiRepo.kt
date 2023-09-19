c241c482484045e8def40093b7b588e1cde2246d
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-09-19 15:39:23 +0100

    DI Implemented with Repository

--- app/src/main/java/com/example/cirlan/data/repository/ApiRepo.kt
+++ app/src/main/java/com/example/cirlan/data/repository/ApiRepo.kt
@@ -4,8 +4,8 @@ import com.example.cirlan.domain.model.driversroutes.DriverModel
 import com.example.cirlan.domain.model.driversroutes.RouteModel
 
 interface ApiRepo {
-
     suspend fun getDrivers(): List<DriverModel?>?
 
     suspend fun getRoutes(): List<RouteModel?>?
+
 }
