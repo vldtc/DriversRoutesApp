93fe787629399291291d2a2a0a06d4ea8f95644c
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-09-19 14:59:11 +0100

    Project Layers created and structured by: Data/Domain/DI/Presentation

--- app/src/main/java/com/example/cirlan/MainActivity.kt
+++ app/src/main/java/com/example/cirlan/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.cirlan.ui.theme.DriversRoutesAppTheme
+import com.example.cirlan.presentation.ui.theme.DriversRoutesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/cirlan/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/cirlan/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.cirlan.ui.theme
+package com.example.cirlan.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
