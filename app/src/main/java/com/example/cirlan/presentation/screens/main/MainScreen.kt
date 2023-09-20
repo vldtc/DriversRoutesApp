20ee30a5c3fe0694981401c4714dac9486a11b0c
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-09-20 14:44:22 +0100

    DriversItem Ui Improved Sort Section Included and Implemented

--- app/src/main/java/com/example/cirlan/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/cirlan/presentation/screens/main/MainScreen.kt
@@ -1,9 +1,11 @@
 package com.example.cirlan.presentation.screens.main
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,17 +15,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -31,7 +40,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cirlan.data.model.localdb.DriversDBModel
-import com.example.cirlan.domain.model.driversroutesmapper.DriverModelMapped
+import com.example.cirlan.domain.usecase.sortListBy
 import com.example.cirlan.presentation.common.LoadingBar
 import com.example.cirlan.presentation.ui.theme.LineColour
 
@@ -40,11 +49,27 @@ fun MainScreen() {
 
     val viewModel = hiltViewModel<MainViewModel>()
     val drivers by viewModel.drivers.collectAsState()
-    val routes by viewModel.routes.collectAsState()
     val loadingState by viewModel.loadingState.collectAsState()
 
     viewModel.getDriversFromDB()
     viewModel.getRoutesFromDB()
+
+    MainScreenContent(drivers, loadingState)
+
+}
+
+@SuppressLint("RememberReturnType")
+@Composable
+fun MainScreenContent(
+    drivers: List<DriversDBModel>,
+    loadingState: Boolean
+) {
+
+    val (sortBy, setSortBy) = remember { mutableStateOf("First\nname") }
+
+    val sortedList = remember(drivers, sortBy) {
+        sortListBy(drivers, sortBy)
+    }
 
     LazyColumn(
         modifier = Modifier
@@ -53,15 +78,17 @@ fun MainScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
+            DriversFilterSection(onSortByChanged = setSortBy)
+        }
+        item {
             DriversLegend()
         }
-        items(drivers.size ?: 0) {
-            DriversItem(drivers[it])
+        items(sortedList.size ?: 0) {
+            DriversItem(sortedList[it])
         }
     }
 
     if (loadingState) LoadingBar()
-
 }
 
 @Composable
@@ -71,7 +98,7 @@ fun DriversItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(40.dp),
+            .height(50.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 6.dp,
             pressedElevation = 0.dp
@@ -81,7 +108,8 @@ fun DriversItem(
         Row(
             modifier = Modifier
                 .fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Box(
                 modifier = Modifier
@@ -94,8 +122,8 @@ fun DriversItem(
             }
             Row(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp),
+                    .padding(horizontal = 8.dp)
+                    .weight(1f),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -114,6 +142,15 @@ fun DriversItem(
                     textAlign = TextAlign.Start
                 )
             }
+            Box(
+                modifier = Modifier
+                    .width(16.dp)
+                    .fillMaxHeight()
+                    .background(LineColour)
+                    .zIndex(1f)// Set the background color
+            ) {
+                Text(text = "")
+            }
         }
     }
     Spacer(modifier = Modifier.height(16.dp))
@@ -124,8 +161,7 @@ fun DriversLegend() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 8.dp)
-            .padding(start = 16.dp),
+            .padding(horizontal = 24.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -147,4 +183,68 @@ fun DriversLegend() {
     Spacer(modifier = Modifier.height(4.dp))
     Divider(thickness = 1.dp, color = LineColour)
     Spacer(modifier = Modifier.height(16.dp))
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun DriversFilterSection(
+    onSortByChanged: (String) -> Unit
+) {
+
+    val radioOptions = listOf("First\nname", "Last\nname", "ID\nnumber")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .selectableGroup(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Filter by",
+            color = LineColour,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 14.sp,
+        )
+        radioOptions.forEach { text ->
+            AnimatedContent(targetState = selectedOption) { selectedOption ->
+                Row(
+                    Modifier
+                        .height(40.dp)
+                        .selectable(
+                            selected = (text == selectedOption),
+                            onClick = {
+                                onOptionSelected(text)
+                                onSortByChanged(text)
+                            },
+                            role = Role.RadioButton
+                        )
+                        .padding(horizontal = 4.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(
+                            if (text == selectedOption) {
+                                MaterialTheme.colorScheme.primary
+                            } else {
+                                MaterialTheme.colorScheme.primaryContainer
+                            }
+                        ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodySmall,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+    Spacer(modifier = Modifier.height(8.dp))
+    Divider(thickness = 1.dp, color = LineColour)
+    Spacer(modifier = Modifier.height(4.dp))
 }
